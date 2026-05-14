ec47a02644a52aa76690ef624e794cdf4c284087
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-14 19:11:29 -0400

    Add playwright for browser manipulation

--- flake.nix
+++ flake.nix
@@ -23,17 +23,24 @@
             python3
             uv
             nodejs # needed for installing pyright w uv
+            playwright-driver.browsers # required for playwright to work (library to manipulate browser w python)
 
             # NIX
             nixd
             self.formatter.${system}
           ];
 
-          env = lib.optionalAttrs pkgs.stdenv.isLinux {
-            # Python libraries often load native shared objects using dlopen(3).
-            # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
-            LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
-          };
+          env =
+            {
+              PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+              PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
+              PLAYWRIGHT_HOST_PLATFORM_OVERRIDE = "ubuntu-24.04";
+            }
+            // lib.optionalAttrs pkgs.stdenv.isLinux {
+              # Python libraries often load native shared objects using dlopen(3).
+              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
 
           shellHook = ''
             unset PYTHONPATH
